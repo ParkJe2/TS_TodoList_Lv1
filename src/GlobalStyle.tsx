c062f3d2925e6eb69cb79f2828ce0071c2502b9a
parkjei <parkjei94@gmail.com>
2023-07-27 23:12:58 +0900

    Design/Change Font

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -4,16 +4,21 @@ import reset from "styled-reset";
 const GlobalStyle = createGlobalStyle`
 ${reset}
 
-@import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;600&display=swap");
+@font-face {
+    font-family: 'SUITE-Regular';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2304-2@1.0/SUITE-Regular.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+}
 
-*{
-  font-family: "Noto Sans KR", sans-serif;
+* {
+  font-family: 'SUITE-Regular';
     outline:none;
 }
 
 body {
   background-color: #f5f5f5;
-  font-family: "Noto Sans KR", sans-serif;
+  font-family: 'SUITE-Regular';
 }
 `;
 
